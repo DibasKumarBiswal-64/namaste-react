42733d34a9bb49666e88ffee688b660b63baf51a
Dibas Kumar Biswal <dibasbiswal4444@gmail.com>
2023-08-11 02:29:06 +0530

    react produ

--- App.js
+++ App.js
@@ -1,3 +1,7 @@
+import React from "react";
+import ReactDOM from "react-dom/client"
+
+
 const parent = React.createElement("div",{id:"parent"},[
     React.createElement("div",{id:"child"},[
         React.createElement("h1",{},"I'm an h1 tag"),
@@ -5,7 +9,7 @@ const parent = React.createElement("div",{id:"parent"},[
     ]),
 ]);
 
-
+console.log(parent);
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(parent);
