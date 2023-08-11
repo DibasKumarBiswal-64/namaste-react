663fad9672eab76c6affa6504e719cb4e168b485
Dibas Kumar Biswal <dibasbiswal4444@gmail.com>
2023-08-11 14:58:24 +0530

    body card

--- App.js
+++ App.js
@@ -1,15 +1,49 @@
 import React from "react";
-import ReactDOM from "react-dom/client"
+import ReactDOM from "react-dom/client";
 
 
-const parent = React.createElement("div",{id:"parent"},[
-    React.createElement("div",{id:"child"},[
-        React.createElement("h1",{},"I'm an h1 tag"),
-        React.createElement("h2",{},"I'm an h2 tag"),
-    ]),
-]);
+const Header = () => {
+  return (  <div className="header">
+        <div className="logo">
+        <img src="https://img.freepik.com/free-vector/food-delivery-logo-with-bike-man-courier_1308-48914.jpg"/>
+        </div>
+        <div className="nav-items">
+            <ul>
+                <li>Home</li>
+                <li>About Us</li>
+                <li>Contact Us</li>
+                <li>Card</li>
+            </ul>
+        </div>
+    </div>
+  );
+};
 
-console.log(parent);
+const RestaurantCard = () => {
+    return (
+        <div className="res-card">
+            <h3>KFC</h3>
+        </div>
+    );
+};
+const Body = () => {
+    return (
+        <div className="body">
+            <div className="Search">Search</div>
+            <div className="res-container">
+                <RestaurantCard/>
+            </div>
+        </div>
+    )
+}
+const AppLayout = () => {
+    return (
+        <div className="app">
+            <Header/>
+            <Body/>
+        </div>
+    );
+};
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(parent);
+root.render(<AppLayout/>);
