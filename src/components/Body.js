f17282ca3df9667cb33c8395a11ac9014b8e78c2
Sachin <sachinsunny303@gmail.com>
2023-08-17 06:29:16 +0100

    fixed data fetch issue

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,48 +1,61 @@
-import RestaurantCard from "../RestaurantCard";
-import resList from "../utils/mockData";
-import { useState,useEffect } from "react";
-
-
+import RestaurantCard from '../RestaurantCard';
+import resList from '../utils/mockData';
+import {useState, useEffect} from 'react';
 
 const Body = () => {
+  //local state variable
 
+  const [ListOfRestaurants, setListOfRestaurants] = useState(resList);
 
-    //local state variable
-
-  const [ListOfRestaurants, setListOfRestaurants ] = useState(resList);
-
-    
-  useEffect(()=>{
+  useEffect(() => {
     fetchData();
-  },[]);
+  }, []);
 
   const fetchData = async () => {
-            const data = await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=20.2508183&lng=85.8145617&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING");
-            const json = await data.json();
-            console.log(json);
-            setListOfRestaurants(json.data.cards[4].card.gridElements.infoWithStyle.restaurants);
+    const data = await fetch(
+      'https://corsproxy.io/?https://www.swiggy.com/dapi/restaurants/list/v5?lat=20.2508183&lng=85.8145617&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING'
+    );
+    const jsonData = await data.json();
+    console.log(jsonData);
+    updateStateVariable(jsonData);
+    console.log(ListOfRestaurants);
   };
 
-    return (
-        <div className="body">
-            <div className="filter"></div>
-            <button className="filter-btn" 
-                onClick={()=>{
-                  const filteredList = ListOfRestaurants.filter((res) => res?.info?.avgRating > 4);
-                  
-                        setListOfRestaurants(filteredList);
-                }}
-                >
-                Top Rated Restaurants
-                </button>
-            <div className="res-container">
-                {
-                  ListOfRestaurants.map((restaurant)=>(
-                  <RestaurantCard key={restaurant?.info?.id} resData={restaurant}/>
-                  ))}
-            </div>
-        </div>
-    );
+  const updateStateVariable = (jsonData) => {
+    jsonData.data.cards.map((item) => {
+      if (item.card.card.id === 'top_brands_for_you') {
+        setListOfRestaurants(
+          item?.card?.card?.gridElements?.infoWithStyle?.restaurants
+        );
+      }
+    });
+  };
+
+  return (
+    <div className="body">
+      <div className="filter"></div>
+      <button
+        className="filter-btn"
+        onClick={() => {
+          const filteredList = ListOfRestaurants.filter(
+            (res) => res?.info?.avgRating > 4
+          );
+
+          setListOfRestaurants(filteredList);
+        }}
+      >
+        Top Rated Restaurants
+      </button>
+      <div className="res-container">
+        {ListOfRestaurants.map((restaurant) => (
+          <RestaurantCard
+            key={restaurant?.info?.id}
+            resData={restaurant}
+          />
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Body;
