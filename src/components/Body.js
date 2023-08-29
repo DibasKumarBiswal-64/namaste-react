abec9e39355c65584396a2bf3ebcd0bd032c58b4
Dibas Kumar Biswal <dibasbiswal4444@gmail.com>
2023-08-29 22:42:33 +0530

    update

--- src/components/Body.js
+++ src/components/Body.js
@@ -2,6 +2,7 @@ import RestaurantCard from "../RestaurantCard";
 import resList from "../utils/mockData";
 import { useState,useEffect } from "react";
 import Shimmer from "./Shimmer";
+import { Link } from "react-router-dom";
 
 
 
@@ -80,7 +81,10 @@ const Body = () => {
             <div className="res-container">
                 {
                   filteredRestaurant.map((restaurant)=>(
-                  <RestaurantCard key={restaurant?.info?.id} resData={restaurant}/>
+                  <Link key={restaurant?.info?.id}
+                   to={"/restaurants/"+restaurant?.info?.id}
+                   >
+                    <RestaurantCard  resData={restaurant}/></Link>
                   ))}
             </div>
         </div>
--- src/utils/contants.js
+++ src/utils/contants.js
@@ -3,3 +3,5 @@ export const CDN_URL ="https://media-assets.swiggy.com/swiggy/image/upload/fl_lo
 export const LOGO_URL ="https://img.freepik.com/free-vector/food-delivery-logo-with-bike-man-courier_1308-48914.jpg";
 
 
+export const MENU_API =
+ "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=20.2508183&lng=85.8145617&restaurantId=";
