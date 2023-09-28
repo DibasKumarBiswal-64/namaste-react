cab419026b83c2232f98572b4efda9173d8f4150
Dibas Kumar Biswal <dibasbiswal4444@gmail.com>
2023-09-28 19:04:12 +0530

    update

--- src/utils/useRestaurantMenu.js
+++ src/utils/useRestaurantMenu.js
@@ -11,7 +11,8 @@ useEffect(() =>{
 const fetchData = async () =>{
     const data = await fetch(MENU_API + resId);
     const json =await data.json();
-    setResInfo(json.data);
+    setResInfo(json?.data);
+    
 };
     return resInfo;
 };
